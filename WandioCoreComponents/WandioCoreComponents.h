844bda51020e0237e6fa8396e975883628fb3f18
Kakhi Kiknadze <kakhi.kiknadze@wandio.com>
2020-10-13 12:06:38 +0400

    initial commit

---
+++ WandioCoreComponents/WandioCoreComponents.h
@@ -0,0 +1,18 @@
+//
+//  WandioCoreComponents.h
+//  WandioCoreComponents
+//
+//  Created by Kakhi Kiknadze on 10/13/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WandioCoreComponents.
+FOUNDATION_EXPORT double WandioCoreComponentsVersionNumber;
+
+//! Project version string for WandioCoreComponents.
+FOUNDATION_EXPORT const unsigned char WandioCoreComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WandioCoreComponents/PublicHeader.h>
+
+
---
+++ WandioCoreComponentsExamples/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WandioCoreComponentsExamples
+//
+//  Created by Kakhi Kiknadze on 10/13/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
